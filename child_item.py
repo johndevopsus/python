bf444d8d30a598e35ab449fd8d4a64c1fb56a5f8
John <102876985+johndevopsus@users.noreply.github.com>
2023-12-14 08:12:08 -0500

    Update child_item.py

--- child_item.py
+++ child_item.py
@@ -118,3 +118,105 @@ try:
     compare_lists(api_tasks, your_list_of_tasks)
 except Exception as e:
     print(f"An error occurred: {e}")
+
+#Python script to get each "Child Item" type is task, title is any, and state status is not "Done" from a specific "workitem" in Azure DevOps Board. I need it as a python list.
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+def get_child_items(work_item_id, organization, project, personal_access_token):
+    # Azure DevOps REST API base URL
+    base_url = f"https://dev.azure.com/{organization}/{project}/_apis"
+
+    # REST API endpoint to get work item details including child items
+    url = f"{base_url}/wit/workitems/{work_item_id}?expand=children"
+
+    # Define request headers with authorization
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {personal_access_token}"
+    }
+
+    # Make the request to get work item details including child items
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        work_item_data = response.json()
+
+        # Extract child items
+        child_items = work_item_data.get("relations", [])
+
+        # Create a list to store child item details
+        child_items_list = []
+
+        # Iterate through child items and extract information
+        for item in child_items:
+            if "attributes" in item and "child" in item["attributes"]:
+                child_item_id = item["attributes"]["child"]["id"]
+                
+                # Get details of the child item
+                child_item_details = get_work_item_details(child_item_id, organization, project, personal_access_token)
+
+                # Append child item details to the list
+                child_items_list.append(child_item_details)
+
+        return child_items_list
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+def get_work_item_details(work_item_id, organization, project, personal_access_token):
+    # Azure DevOps REST API base URL
+    base_url = f"https://dev.azure.com/{organization}/{project}/_apis"
+
+    # REST API endpoint to get work item details
+    url = f"{base_url}/wit/workitems/{work_item_id}"
+
+    # Define request headers with authorization
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {personal_access_token}"
+    }
+
+    # Make the request to get work item details
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        work_item_data = response.json()
+
+        # Extract work item type, title, and status
+        item_type = work_item_data["fields"].get("System.WorkItemType", "")
+        title = work_item_data["fields"].get("System.Title", "")
+        status = work_item_data["fields"].get("System.State", "")
+
+        # Return a dictionary with work item details
+        return {
+            "Type": item_type,
+            "Title": title,
+            "Status": status
+        }
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+# Replace these values with your Azure DevOps organization, project, and personal access token
+organization = "YourOrganization"
+project = "YourProject"
+personal_access_token = "YourPersonalAccessToken"
+work_item_id = "YourWorkItemID"
+
+# Get child items details for a specific work item
+child_items_list = get_child_items(work_item_id, organization, project, personal_access_token)
+
+# Print the list of child items
+if child_items_list:
+    for child_item in child_items_list:
+        print(child_item)
+
